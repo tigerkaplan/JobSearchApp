a67df72c0e431d6e4b0e6641721907c80de7eeb9
tigerkaplan <husniyeerparun@gmail.com>
2023-12-30 22:24:39 +0000

    update

--- api/config.js
+++ api/config.js
@@ -1,6 +1,8 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 module.exports = {
   // environment variables
   APP_ID: process.env.APP_ID,
   API_KEY: process.env.API_KEY,
-  BASE_URL: "https://api.adzuna.com/v1/api/jobs/gb/search/1",  //API from https://api.adzuna.com/v1/api/
+  BASE_URL: "https://api.adzuna.com/v1/api/jobs",  //API from https://api.adzuna.com/v1/api/
 };
--- api/index.js
+++ api/index.js
@@ -1,41 +1,42 @@
-const createServer = require("http").createServer;
-const url = require("url");
-const axios = require("axios");
-const chalk = require("chalk");
-const config = require("./config.js");
+const createServer = require('http').createServer;
+const url = require('url');
+const axios = require('axios');
+const chalk = require('chalk');
+const config = require('./config');
 
-const header = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET"
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET',
 };
-
-// ?search=php&location=london
-
-const decodeParams = searchParams =>
-  Array.from(searchParams.keys()).reduce(
-    (acc, key) => ({ ...acc, [key]: searchParams.get(key) }),
-    {}
-  );
 
 const server = createServer((req, res) => {
   const requestURL = url.parse(req.url);
-  // { search: "php", location: "london"}
   const decodedParams = decodeParams(new URLSearchParams(requestURL.search));
-  const {search, location, country = "gb"} = decodedParams;
+  const { search, location, country = 'gb' }  = decodedParams;
 
-  const targetURL= `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
-  if (req.method === "GET") {
-    console.log(chalk.green(`Proxy GET request to : ${targetURL}`));
-    axios.get (targetURL)
-    .then(response => {
-      res.writeHead(200, headers);
-      res.end(JSON.stringify(response.data));
-    })
-    .cath(error => {
-      console.log(chalk.red(error));
-      res.writeHead(500, headers);
-      res.end(JSON.stringify(error));
-    });
-  }
+  const targetURL = `${config.BASE_URL}/${country.toLowerCase()}/${config.BASE_PARAMS}&app_id=${config.APP_ID}&app_key=${config.API_KEY}&what=${search}&where=${location}`;
+    if (req.method === 'GET') {
+      console.log(chalk.green(`Proxy GET request to : ${targetURL}`));
+      axios.get(targetURL)
+        .then(response => {
+          res.writeHead(200, headers);
+          res.end(JSON.stringify(response.data));
+        })
+        .catch(response => {
+          console.log(chalk.red(response));
+          res.writeHead(500, headers);
+          res.end(JSON.stringify(response));
+        });
+    } 
 });
+
+
+server.listen(3000, () => {
+  console.log(chalk.green('Server listening'));
+} );
+
+
+const decodeParams = searchParams => Array
+  .from(searchParams.keys())
+  .reduce((acc, key) => ({ ...acc, [key]: searchParams.get(key) }), {});
