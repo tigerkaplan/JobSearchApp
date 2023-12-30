1b8f8341c45da5b79fe501af06342e254935bfb0
tigerkaplan <husniyeerparun@gmail.com>
2023-12-30 18:07:40 +0000

    module. exports added

--- api/config.js
+++ api/config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  
-}
+  // environment variables
+  APP_ID: process.env.APP_ID,
+  API_KEY: process.env.API_KEY,
+  BASE_URL: "https://api.adzuna.com/v1/api/jobs/gb/search/1",  //API from https://api.adzuna.com/v1/api/
+};
